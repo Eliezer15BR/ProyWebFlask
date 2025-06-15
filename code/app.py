c53bd5ba6793d1d7a5bf9ebd4930e07a563285b7
Eliezer <eeliezercm2@gmail.com>
2025-06-15 10:40:45 -0400

    Optimizacion y adecuación a bd

--- code/app.py
+++ code/app.py
@@ -14,6 +14,8 @@ mysql=MySQL(app)
 def index():
     return render_template('index.html')
 
+
+#######CATEGORIA################
 @app.route('/categorias')
 def categorias():
     cursor=mysql.connection.cursor()
@@ -28,7 +30,7 @@ def editar_categoria(codigo_categoria):
     cursor.execute("SELECT * FROM categoria WHERE codigo_categoria= %s ",(codigo_categoria,))
     datos=cursor.fetchall()
     cursor.close()
-    return render_template("editaCategoria.html",categoria=datos[0])
+    return render_template("formCategoria.html",categoria=datos[0])
 
 @app.route('/actualiza_categoria/<codigo_categoria>',methods=['POST'])
 def actualiza_categoria(codigo_categoria):
@@ -42,22 +44,18 @@ def actualiza_categoria(codigo_categoria):
 
 @app.route('/nueva_categoria')
 def nueva_categoria():
-    return render_template("nuevaCategoria.html")
+    return render_template("formCategoria.html")
 
 @app.route('/agrega_categoria',methods=['POST'])
 def agrega_categoria():
     if request.method=='POST':
-        codigo_categoria=request.form['codigo_categoria']
         nombre_categoria=request.form['nombre_categoria']
         cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM categoria WHERE codigo_categoria=%s",(codigo_categoria,))
-        duplicado=cursor.fetchone()
-        if duplicado:
-            return render_template("errorCodigo.html")
-        cursor.execute("INSERT INTO categoria VALUES(%s,%s)",(codigo_categoria,nombre_categoria,))
+        cursor.execute("INSERT INTO categoria(nombre_categoria) VALUES(%s)",(nombre_categoria,))
         mysql.connection.commit()
         cursor.close()
         return redirect(url_for('categorias'))
+    
 @app.route('/eliminar_categoria/<codigo_categoria>')
 def eliminar_categoria(codigo_categoria):
     cursor=mysql.connection.cursor()
@@ -68,9 +66,12 @@ def eliminar_categoria(codigo_categoria):
 
 
 
+
+############SUBCATEGORIAS####################
+
 @app.route('/subcategorias')
-@app.route('/subcategorias/<codigo_categoria>')
-def subcategorias(codigo_categoria=None):
+@app.route('/subcategorias/<codigo_categoria>/<nombre_categoria>')
+def subcategorias(codigo_categoria=None,nombre_categoria=None):
     cursor=mysql.connection.cursor()
     query="""
             SELECT * FROM categoria 
@@ -85,7 +86,7 @@ def subcategorias(codigo_categoria=None):
     cursor.execute(query,params)
     datos=cursor.fetchall()
     cursor.close()
-    return render_template('subcategorias.html',subcategorias=datos)
+    return render_template('subcategorias.html',subcategorias=datos,nombre_categoria=nombre_categoria)
 
 @app.route('/editar_subcategoria/<codigo_subcategoria>')
 def editar_subcategoria(codigo_subcategoria):
@@ -95,7 +96,7 @@ def editar_subcategoria(codigo_subcategoria):
     cursor.execute("SELECT * FROM categoria")
     categorias=cursor.fetchall()
     cursor.close()
-    return render_template("editaSubcategoria.html",subcategoria=subcategoria[0],categorias=categorias)
+    return render_template("formSubcategoria.html",subcategoria=subcategoria[0],categorias=categorias)
 
 @app.route('/actualiza_subcategoria/<codigo_subcategoria>',methods=['POST'])
 def actualiza_subcategoria(codigo_subcategoria):
@@ -114,20 +115,15 @@ def nueva_subcategoria():
     cursor.execute("SELECT * FROM categoria")
     categorias=cursor.fetchall()
     cursor.close()
-    return render_template("nuevaSubcategoria.html",categorias=categorias)
+    return render_template("formSubcategoria.html",categorias=categorias)
 
 @app.route('/agrega_subcategoria',methods=['POST'])
 def agrega_subcategoria():
     if request.method=='POST':
-        codigo_subcategoria=request.form['codigo_subcategoria']
         nombre_subcategoria=request.form['nombre_subcategoria']
         codigo_categoria=request.form['codigo_categoria']
         cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM subcategoria WHERE codigo_subcategoria=%s",(codigo_subcategoria,))
-        duplicado=cursor.fetchone()
-        if duplicado:
-            return render_template("errorCodigo.html")
-        cursor.execute("INSERT INTO subcategoria VALUES(%s,%s,%s)",(codigo_subcategoria,nombre_subcategoria,codigo_categoria,))
+        cursor.execute("INSERT INTO subcategoria(nombre_subcategoria,codigo_categoria) VALUES(%s,%s)",(nombre_subcategoria,codigo_categoria,))
         mysql.connection.commit()
         cursor.close()
         return redirect(url_for('subcategorias'))
@@ -140,10 +136,14 @@ def eliminar_subcategoria(codigo_subcategoria):
     cursor.close()
     return redirect(url_for('subcategorias'))    
 
+
+
+
+##########ARTICULOS########################
 @app.route('/articulos')
-@app.route('/articulos/subcategoria/<codigo_subcategoria>',endpoint='articulos_por_subcategoria')
-@app.route('/articulos/categoria/<codigo_categoria>',endpoint='articulos_por_categoria')
-def articulos(codigo_subcategoria=None,codigo_categoria=None):
+@app.route('/articulos/subcategoria/<codigo_subcategoria>/<nombre_subcategoria>/<nombre_categoria>',endpoint='articulos_por_subcategoria')
+@app.route('/articulos/categoria/<codigo_categoria>/<nombre_categoria>',endpoint='articulos_por_categoria')
+def articulos(codigo_subcategoria=None,nombre_subcategoria=None,codigo_categoria=None,nombre_categoria=None):
     cursor=mysql.connection.cursor()
     query="""
         SELECT * FROM articulo 
@@ -160,7 +160,7 @@ def articulos(codigo_subcategoria=None,codigo_categoria=None):
     cursor.execute(query,params)
     datos=cursor.fetchall()
     cursor.close()
-    return render_template('articulos.html',articulos=datos)
+    return render_template('articulos.html',articulos=datos,nombre_categoria=nombre_categoria,nombre_subcategoria=nombre_subcategoria)
 
 @app.route('/editar_articulo/<codigo_articulo>/<codigo_categoria>')
 def editar_articulo(codigo_articulo,codigo_categoria):
@@ -172,7 +172,7 @@ def editar_articulo(codigo_articulo,codigo_categoria):
     cursor.execute("SELECT * FROM subcategoria WHERE codigo_categoria=%s",(codigo_categoria))
     subcategorias=cursor.fetchall()
     cursor.close()
-    return render_template("editaArticulo.html",articulo=articulo[0],categorias=categorias,subcategorias=subcategorias)
+    return render_template("formArticulo.html",articulo=articulo[0],categorias=categorias,subcategorias=subcategorias)
 
 @app.route('/get_subcategorias/<codigo_categoria>')
 def get_subcategorias(codigo_categoria):
@@ -200,21 +200,16 @@ def nuevo_articulo():
     cursor.execute("SELECT * FROM categoria")
     categorias=cursor.fetchall()
     cursor.close()
-    return render_template("nuevoArticulo.html",categorias=categorias)
+    return render_template("formArticulo.html",categorias=categorias)
     
 @app.route('/agrega_articulo',methods=['POST'])
 def agrega_articulo():
     if request.method=='POST':
-        codigo_articulo=request.form['codigo_articulo']
         nombre_articulo=request.form['nombre_articulo']
         codigo_subcategoria=request.form['codigo_subcategoria']
         codigo_categoria=request.form['codigo_categoria']
         cursor=mysql.connection.cursor()
-        cursor.execute("SELECT * FROM articulo WHERE codigo_articulo=%s",(codigo_articulo,))
-        duplicado=cursor.fetchone()
-        if duplicado:
-            return render_template("errorCodigo.html")
-        cursor.execute("INSERT INTO articulo VALUES(%s,%s,%s,%s)",(codigo_articulo,nombre_articulo,codigo_categoria,codigo_subcategoria,))
+        cursor.execute("INSERT INTO articulo(nombre_articulo,codigo_categoria,codigo_subcategoria) VALUES(%s,%s,%s)",(nombre_articulo,codigo_categoria,codigo_subcategoria,))
         mysql.connection.commit()
         cursor.close()
         return redirect(url_for('articulos'))
